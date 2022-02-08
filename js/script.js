8576bb3c917b52b5ead595293f8c76a4c1166ce4
Damian <damian.ludwig83@gmail.com>
2022-02-08 10:03:01 +0100

    add js scripts

---
+++ js/script.js
@@ -0,0 +1,67 @@
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+const cityName = document.querySelector('.city-name')
+const photo = document.querySelector('.photo')
+const warning = document.querySelector('.warning')
+const weather = document.querySelector('.weather-result')
+const temperature = document.querySelector('.temperature-result')
+const humidity = document.querySelector('.humidity-result')
+
+const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q='
+
+const API_KEY = '&appid=2baecb1aaa8e38504cc150973fa76472'
+
+const API_UNITS = '&units=metric '
+
+const checkWeather = () => {
+    const city = input.value || 'Warsaw'
+    const URL = API_LINK + city + API_KEY + API_UNITS
+
+    axios.get(URL).then(res => {
+        const status = Object.assign({}, ...res.data.weather)
+
+        cityName.textContent = res.data.name
+        cityName.classList.remove('warning')
+        temperature.textContent = Math.floor(res.data.main.temp) + ' °C'
+        humidity.textContent = res.data.main.humidity + ' %'
+        weather.textContent = status.main
+
+        input.value = ''
+
+        photo.setAttribute('src', '/img/Cloud.png')
+
+        if (status.id >= 200 && status.id < 300) {
+            photo.setAttribute('src', '/img/Cloud & Lightning.png')
+        } else if (status.id >= 300 && status.id < 400) {
+            photo.setAttribute('src', '/img/Sun Behind Clouds.png')
+        } else if (status.id >= 500 && status.id < 600) {
+            photo.setAttribute('src', '/img/Rain with Clouds.png')
+        } else if (status.id >= 600 && status.id < 700) {
+            photo.setAttribute('src', '/img/Heavy Snow Rain.png')
+        } else if (status.id >= 701 && status.id < 800) {
+            photo.setAttribute('src', '/img/Sun Behind Clouds.png')
+        } else if (status.id === 800) {
+            photo.setAttribute('src', '/img/Lighter Heat.png')
+        } else if (status.id >= 801 && status.id < 900) {
+            photo.setAttribute('src', '/img/Sun Behind Clouds.png')
+        }
+
+    }).catch(() => {
+        cityName.classList.add('warning')
+        cityName.textContent = 'Wpisz poprawną nazwę miasta !'
+        photo.setAttribute('src', '')
+        temperature.textContent = ''
+        humidity.textContent = ''
+        weather.textContent = ''
+    })
+}
+
+const checkEnter = e => {
+    if (e.key === 'Enter') {
+        checkWeather()
+    }
+}
+
+input.addEventListener('keyup', checkEnter)
+
+button.addEventListener('click', checkWeather)
